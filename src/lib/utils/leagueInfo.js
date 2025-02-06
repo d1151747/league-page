8d7997096a37c394cf1d9029eca29a2b950bdd02
d1151747 <xbxgmr123@gmail.com>
2025-02-06 18:31:48 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -32,7 +32,7 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Washington, D.C.", // (optional)
       "bio": "Passionate member of Bills Mafia",
-      "photo": "/managers/Josh allen.png", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/declanwatson.jpeg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": null, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "buf", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
